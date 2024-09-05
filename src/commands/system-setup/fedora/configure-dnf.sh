2ecae990185c906e75a6b9d5131fd1cf8d22c47b
Angaddeep Singh <deepangad14@gmail.com>
2024-09-06 00:33:19 +0530

    Small Fix

--- src/commands/system-setup/fedora/configure-dnf.sh
+++ src/commands/system-setup/fedora/configure-dnf.sh
@@ -5,7 +5,7 @@
 configureDNF() {
     case $PACKAGER in
         dnf)
-            echo "max_parallel_downloads=10" | $ESCALATION_TOOL tee -a /etc/dnf/dnf.conf > /dev/null
+            $ESCALATION_TOOL sed -i '/^max_parallel_downloads=/c\max_parallel_downloads=10' /etc/dnf/dnf.conf || echo 'max_parallel_downloads=10' >> /etc/dnf/dnf.conf
             echo "fastestmirror=True" | $ESCALATION_TOOL tee -a /etc/dnf/dnf.conf > /dev/null
             echo "defaultyes=True" | $ESCALATION_TOOL tee -a /etc/dnf/dnf.conf > /dev/null
             
